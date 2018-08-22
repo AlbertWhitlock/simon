35a6f30cbd7b20e46aaa5c1e9a3cfdd06d33ee3f
Albert <albertwhitlock4@gmail.com>
2018-08-21 22:07:38 -0400

    Slowed up blinking of buttons

--- script.js
+++ script.js
@@ -37,29 +37,82 @@ function colorButtonClicked(evt) {
   }
 }
 
-//'Main' function. Will call other functions to get things going
-function playGame(evt) {
-  resetArrays();
-  startGame(currentRound);
-  printResults();
-}
+//Will start the game, get random buttons, and call the method to make them blink
+function startGame(round) {
+  console.log("***********************Current round is: " + round);
+  document.getElementById("roundNumber").innerHTML = round;
+  numMoves = checkNumMoves(round);
+  var timeoutTime = 0;
+  var timeoutTimeDim = 0;
 
-//Clear the arrays between games
-function resetArrays() {
-  responseArray = [];
-  solutionArray = [];
-}
+  for (let i = 1; i <= numMoves; i++) {
+    var arrayIndex = i - 1;
+    timeoutTime = timeoutTime + 700;
+    timeoutTimeDim = timeoutTime + 300;
+    var randomButton =
+      buttonArray[Math.floor(Math.random() * buttonArray.length)];
 
-function wait() {
-  var startTime = new Date();
-  var currentTime = new Date();
-  while (currentTime - startTime < 800) {
-    currentTime = new Date();
+    if (randomButton == redButton) {
+      solutionArray[arrayIndex] = "red";
+      lightUpButton("red", timeoutTime);
+      dimDownButton("red", timeoutTimeDim);
+    } else if (randomButton == blueButton) {
+      solutionArray[arrayIndex] = "blue";
+      lightUpButton("blue", timeoutTime);
+      dimDownButton("blue", timeoutTimeDim);
+    } else if (randomButton == yellowButton) {
+      solutionArray[arrayIndex] = "yellow";
+      lightUpButton("yellow", timeoutTime);
+      dimDownButton("yellow", timeoutTimeDim);
+    } else if (randomButton == greenButton) {
+      solutionArray[arrayIndex] = "green";
+      lightUpButton("green", timeoutTime);
+      dimDownButton("green", timeoutTimeDim);
+    }
   }
 }
 
-//When user submits their answer this will see if they are correct
-// and alert them the result
+function lightUpButton(button, time) {
+  if (button == "red") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(255,0,0,.7)";
+    }, time);
+  } else if (button == "blue") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(0,0,255,.7)";
+    }, time);
+  } else if (button == "yellow") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(255,255,0,.7)";
+    }, time);
+  } else if (button == "green") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(0,255,0,.7)";
+    }, time);
+  }
+}
+
+function dimDownButton(button, time) {
+  if (button == "red") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(255,0,0,.2)";
+    }, time);
+  } else if (button == "blue") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(0,0,255,.2)";
+    }, time);
+  } else if (button == "yellow") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(255,255,0,.2)";
+    }, time);
+  } else if (button == "green") {
+    setTimeout(() => {
+      document.getElementById(button).style.background = "rgba(0,255,0,.2)";
+    }, time);
+  }
+}
+
+//When user submits their answer this will see if they are correct and alert them the result
 function submitAnswer(evt) {
   var numCorrectAnswers = 0;
   var numIncorrectAnswers = 0;
@@ -95,99 +148,6 @@ function submitAnswer(evt) {
   else alert("You must first play a game");
 }
 
-//Will start the game, get random buttons, and call the method to make them blink
-function startGame(round) {
-  console.log("***********************Current round is: " + round);
-  document.getElementById("roundNumber").innerHTML = round;
-  numMoves = checkNumMoves(round);
-
-  for (let i = 1; i <= numMoves; i++) {
-    var arrayIndex = i - 1;
-    var randomButton =
-      buttonArray[Math.floor(Math.random() * buttonArray.length)];
-    console.log(randomButton);
-
-    if (randomButton == redButton) {
-      console.log("Inside red");
-      solutionArray[arrayIndex] = "red";
-      setTimeout(function() {
-        buttonBlink("red");
-      }, 800);
-      //buttonBlink("red");
-    } else if (randomButton == blueButton) {
-      console.log("Inside blue");
-      solutionArray[arrayIndex] = "blue";
-      setTimeout(function() {
-        buttonBlink("blue");
-      }, 800);
-    } else if (randomButton == yellowButton) {
-      console.log("Inside yellow");
-      solutionArray[arrayIndex] = "yellow";
-      setTimeout(function() {
-        buttonBlink("yellow");
-      }, 800);
-    } else if (randomButton == greenButton) {
-      console.log("Inside green");
-      solutionArray[arrayIndex] = "green";
-      setTimeout(function() {
-        buttonBlink("green");
-      }, 800);
-    }
-  }
-}
-
-//Will "blink" the button on the screen
-function buttonBlink(button) {
-  console.log("Inside buttonBlink. Button color is: " + button);
-  document.getElementById(button).style.background = "gray";
-  setTimeout(function() {
-    document.getElementById(button).style.backgroundColor = `${button}`;
-  }, 800);
-}
-
-/*
-function flashRed(button) {
-  console.log("Inside flashRed");
-  document.getElementById(button).style.background = "blue";
-  //redButton.style.setProperty("background-color", "#FF0000");
-  //setProperty("redButton", "background-color", "#FF0000");
-  wait(250);
-  document.getElementById(button).style.backgroundColor = "#990000";
-  //setProperty("redButton", "background-color", "#990000");
-  wait(100);
-}
-
-function flashBlue() {
-  console.log("Inside flashBlue");
-  document.getElementById("blue").style.background = "#0000FF";
-  //setProperty("blueButton", "background-color", "#0000FF");
-  wait(250);
-  document.getElementById("blue").style.background = "#000099";
-  //setProperty("blueButton", "background-color", "#000099");
-  wait(100);
-}
-
-function flashYellow() {
-  console.log("Inside flashYellow");
-  yellowButton.style.backgroundColor = "#FFFF00";
-  //setProperty("yellowButton", "background-color", "#FFFF00");
-  wait(250);
-  yellowButton.style.backgroundColor = "#999900";
-  //setProperty("yellowButton", "background-color", "#999900");
-  wait(100);
-}
-
-function flashGreen() {
-  console.log("Inside flashGreen");
-  greenButton.style.backgroundColor = "#00FF00";
-  //setProperty("greenButton", "background-color", "#00FF00");
-  wait(250);
-  greenButton.style.backgroundColor = "#009900";
-  //setProperty("greenButton", "background-color", "#009900");
-  wait(100);
-}
-*/
-
 //Takes what round we are in and returns number of moves
 function checkNumMoves(currentRound) {
   if (currentRound == 1) return 3;
@@ -200,4 +160,17 @@ function checkNumMoves(currentRound) {
 // Debug to the console
 function printResults() {
   console.log("Solution array is " + solutionArray);
+}
+
+//Clear the arrays between games
+function resetArrays() {
+  responseArray = [];
+  solutionArray = [];
+}
+
+//'Main' function. Will call other functions to get things going
+function playGame(evt) {
+  resetArrays();
+  startGame(currentRound);
+  printResults();
 }
